47df63c52fd3d2a0c227f6a3280a7d4a1ef71ac6
GreenUserInq <and.red.alpin@gmail.com>
2024-09-22 11:31:57 +0300

    SensorComm Add

--- PublisherMQTT/obj/Debug/net8.0/PublisherMQTT.AssemblyInfo.cs
+++ PublisherMQTT/obj/Debug/net8.0/PublisherMQTT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PublisherMQTT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+388fd88214f08baecc9292425d90495106417ade")]
 [assembly: System.Reflection.AssemblyProductAttribute("PublisherMQTT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PublisherMQTT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
